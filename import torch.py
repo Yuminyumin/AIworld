b9eb24a0f2b63c1abf1afb3d005fba62beaee9bb
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-04-27 18:56:08 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -1,11 +1,7 @@
 import torch
-import numpy as np
 
-t = np.array([[[0, 1, 2],
-               [3, 4, 5]],
-              [[6, 7, 8],
-               [9, 10, 11]]])
-ft = torch.FloatTensor(t)
+x = torch.FloatTensor([[1, 2], [3, 4]])
+y = torch.FloatTensor([[5, 6], [7, 8]])
 
-print(ft.view([-1, 1, 3]))
-print(ft.view([-1, 1, 3]).shape)
+#dim = 0 은 첫번째 차원을 늘리라는 의미
+print(torch.cat([x, y], dim=0))
