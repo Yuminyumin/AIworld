683e36dae86ce7ee8a9bc922e5aa5b85458913fc
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-01 11:49:23 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -4,9 +4,9 @@ import torch
 #print(torch.ones_like(x))
 #print(torch.zeros_like(x))
 
-x = torch.FloatTensor([[1, 2], [3, 4]])
+x = torch.FloatTensor([1,2])
 
-print(x.mul(2.))
-x=x.mul(2.)
-print(x.mul_(3))
+#print(x.mul(2.))
+#x=x.mul(2.)
+#print(x.mul_(3))
 print(x) 
