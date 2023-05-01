c7e95643b3e81c63a1467f0af121b144fe5668c2
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-01 12:26:22 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -1,12 +1,18 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-#x = torch.FloatTensor([[0, 1, 2], [2, 1, 0]])
-#print(torch.ones_like(x))
-#print(torch.zeros_like(x))
+# 현재 실습하고 있는 파이썬 코드를 재실행해도 다음에도 같은 결과가 나오도록 랜덤 시드(random seed)를 줍니다.
+torch.manual_seed(1)
 
-x = torch.FloatTensor([1,2])
+x_train = torch.FloatTensor([[1], [2], [3]])
+y_train = torch.FloatTensor([[2], [4], [6]])
 
-#print(x.mul(2.))
-#x=x.mul(2.)
-#print(x.mul_(3))
-print(x) 
+
+# 가중치 W를 0으로 초기화하고 학습을 통해 값이 변경되는 변수임을 명시함.
+W = torch.zeros(1, requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+# 가중치 W를 출력
+print(f"W:{W}")
+print(f"b:{b}")
