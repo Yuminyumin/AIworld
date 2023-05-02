dbf58f5146c7c9a15893e02989a38a06b518ddd7
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-02 12:20:39 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -5,39 +5,40 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
-# 훈련 데이터
-x_train  =  torch.FloatTensor([[73,  80,  75], 
-                               [93,  88,  93], 
-                               [89,  91,  80], 
-                               [96,  98,  100],   
-                               [73,  66,  70]])  
-y_train  =  torch.FloatTensor([[152],  [185],  [180],  [196],  [142]])
+# 데이터
+x_train = torch.FloatTensor([[1], [2], [3]])
+y_train = torch.FloatTensor([[2], [4], [6]])
 
-# 가중치와 편향 선언
-W = torch.zeros((3, 1), requires_grad=True)
-b = torch.zeros(1, requires_grad=True)
+# 모델을 선언 및 초기화. 단순 선형 회귀이므로 input_dim=1, output_dim=1.
+model = nn.Linear(1,1)
 
-hypothesis = x_train.matmul(W) + b
+print(list(model.parameters()))
 
-# optimizer 설정
-optimizer = optim.SGD([W, b], lr=1e-5)
+# optimizer 설정. 경사 하강법 SGD를 사용하고 learning rate를 의미하는 lr은 0.01
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-
-nb_epochs = 20
-for epoch in range(nb_epochs + 1):
+# 전체 훈련 데이터에 대해 경사 하강법을 2,000회 반복
+nb_epochs = 2000
+for epoch in range(nb_epochs+1):
 
     # H(x) 계산
-    # 편향 b는 브로드 캐스팅되어 각 샘플에 더해집니다.
-    hypothesis = x_train.matmul(W) + b
+    prediction = model(x_train)
 
     # cost 계산
-    cost = torch.mean((hypothesis - y_train) ** 2)
+    cost = F.mse_loss(prediction, y_train) # <== 파이토치에서 제공하는 평균 제곱 오차 함수
 
-    # cost로 H(x) 개선
+    # cost로 H(x) 개선하는 부분
+    # gradient를 0으로 초기화
     optimizer.zero_grad()
-    cost.backward()
+
+    # 비용 함수를 미분하여 gradient 계산
+    cost.backward() # backward 연산
+    
+    # W와 b를 업데이트
     optimizer.step()
 
-    print('Epoch {:4d}/{} hypothesis: {} Cost: {:.6f}'.format(
-        epoch, nb_epochs, hypothesis.squeeze().detach(), cost.item()
-    ))
+    if epoch % 100 == 0:
+    # 100번마다 로그 출력
+      print('Epoch {:4d}/{} Cost: {:.6f}'.format(
+          epoch, nb_epochs, cost.item()
+      ))
