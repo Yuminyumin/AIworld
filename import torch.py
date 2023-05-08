787b1991679fc856551c373bde5eabba14b88596
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-08 11:20:15 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -23,7 +23,7 @@ class MultivariateLinearRegressionModel(nn.Module):
 model = MultivariateLinearRegressionModel()
 
 # optimizer 설정. 경사 하강법 SGD를 사용하고 learning rate를 의미하는 lr은 0.01
-optimizer = torch.optim.SGD(model.parameters(), lr=0.00005)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.00001)
 
 # 전체 훈련 데이터에 대해 경사 하강법을 2,000회 반복
 nb_epochs = 2000
