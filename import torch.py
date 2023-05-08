e60fd0d3de5fab756e40c66e26bfdf6d9f19e0f1
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-08 11:44:02 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -1,50 +1,49 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
-torch.manual_seed(1)
+from torch.utils.data import TensorDataset # 텐서데이터셋
+from torch.utils.data import DataLoader # 데이터로더
 
-# 데이터
-x_train = torch.FloatTensor([[73, 80, 75],
-                             [93, 88, 93],
-                             [89, 91, 90],
-                             [96, 98, 100],
-                             [73, 66, 70]])
-y_train = torch.FloatTensor([[152], [185], [180], [196], [142]])
-    
-class MultivariateLinearRegressionModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.linear = nn.Linear(3, 1) # 다중 선형 회귀이므로 input_dim=3, output_dim=1.
+x_train  =  torch.FloatTensor([[73,  80,  75], 
+                               [93,  88,  93], 
+                               [89,  91,  90], 
+                               [96,  98,  100],   
+                               [73,  66,  70]])  
+y_train  =  torch.FloatTensor([[152],  [185],  [180],  [196],  [142]])
 
-    def forward(self, x):
-        return self.linear(x)
-model = MultivariateLinearRegressionModel()
+dataset = TensorDataset(x_train, y_train)
 
-# optimizer 설정. 경사 하강법 SGD를 사용하고 learning rate를 의미하는 lr은 0.01
-optimizer = torch.optim.SGD(model.parameters(), lr=0.00001)
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
 
-# 전체 훈련 데이터에 대해 경사 하강법을 2,000회 반복
-nb_epochs = 2000
-for epoch in range(nb_epochs+1):
+#옵티마이저 , 모델 설계
+model = nn.Linear(3,1)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
 
+nb_epochs = 20
+for epoch in range(nb_epochs + 1):
+  for batch_idx, samples in enumerate(dataloader):
+    # print(batch_idx)
+    # print(samples)
+    x_train, y_train = samples
     # H(x) 계산
     prediction = model(x_train)
 
     # cost 계산
-    cost = F.mse_loss(prediction, y_train) # <== 파이토치에서 제공하는 평균 제곱 오차 함수
+    cost = F.mse_loss(prediction, y_train)
 
-    # cost로 H(x) 개선하는 부분
-    # gradient를 0으로 초기화
+    # cost로 H(x) 계산
     optimizer.zero_grad()
-    # 비용 함수를 미분하여 gradient 계산
-    cost.backward() # backward 연산
-    # W와 b를 업데이트
+    cost.backward()
     optimizer.step()
 
-    if epoch % 100 == 0:
-    # 100번마다 로그 출력
-      print('Epoch {:4d}/{} Cost: {:.6f}'.format(
-          epoch, nb_epochs, cost.item()
-      ))
+    print('Epoch {:4d}/{} Batch {}/{} Cost: {:.6f}'.format(
+        epoch, nb_epochs, batch_idx+1, len(dataloader),
+        cost.item()
+        ))
+    
+# 임의의 입력 [73, 80, 75]를 선언
+new_var =  torch.FloatTensor([[73, 80, 75]]) 
+# 입력한 값 [73, 80, 75]에 대해서 예측값 y를 리턴받아서 pred_y에 저장
+pred_y = model(new_var) 
+print("훈련 후 입력이 73, 80, 75일 때의 예측값 :", pred_y) 
