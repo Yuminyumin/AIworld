1daf4b1d14f8186a7976cb90b3d25e5f8800991b
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-08 10:23:11 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -12,19 +12,25 @@ x_train = torch.FloatTensor([[73, 80, 75],
                              [96, 98, 100],
                              [73, 66, 70]])
 y_train = torch.FloatTensor([[152], [185], [180], [196], [142]])
+    
+class MultivariateLinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(3, 1) # 다중 선형 회귀이므로 input_dim=3, output_dim=1.
 
-# 모델을 선언 및 초기화. 다중 선형 회귀이므로 input_dim=3, output_dim=1.
-model = nn.Linear(3,1)
+    def forward(self, x):
+        return self.linear(x)
+model = MultivariateLinearRegressionModel()
 
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
+# optimizer 설정. 경사 하강법 SGD를 사용하고 learning rate를 의미하는 lr은 0.01
+optimizer = torch.optim.SGD(model.parameters(), lr=0.00005)
 
-
+# 전체 훈련 데이터에 대해 경사 하강법을 2,000회 반복
 nb_epochs = 2000
 for epoch in range(nb_epochs+1):
 
     # H(x) 계산
     prediction = model(x_train)
-    # model(x_train)은 model.forward(x_train)와 동일함.
 
     # cost 계산
     cost = F.mse_loss(prediction, y_train) # <== 파이토치에서 제공하는 평균 제곱 오차 함수
@@ -33,7 +39,7 @@ for epoch in range(nb_epochs+1):
     # gradient를 0으로 초기화
     optimizer.zero_grad()
     # 비용 함수를 미분하여 gradient 계산
-    cost.backward()
+    cost.backward() # backward 연산
     # W와 b를 업데이트
     optimizer.step()
 
@@ -42,10 +48,3 @@ for epoch in range(nb_epochs+1):
       print('Epoch {:4d}/{} Cost: {:.6f}'.format(
           epoch, nb_epochs, cost.item()
       ))
-
-# 임의의 입력 [73, 80, 75]를 선언
-new_var =  torch.FloatTensor([[73, 80, 75]]) 
-# 입력한 값 [73, 80, 75]에 대해서 예측값 y를 리턴받아서 pred_y에 저장
-pred_y = model(new_var) 
-print("훈련 후 입력이 73, 80, 75일 때의 예측값 :", pred_y)
-print(list(model.parameters()))
