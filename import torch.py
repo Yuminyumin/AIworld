f98b5a7ac36f28234c52e4c8701cd91f77e9d948
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-11 18:15:59 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -1,13 +1,14 @@
-import numpy as np # 넘파이 사용
-import matplotlib.pyplot as plt # 맷플롯립사용
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-def sigmoid(x): # 시그모이드 함수 정의
-    return 1/(1+np.exp(-x))
+torch.manual_seed(1)
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
+x_data = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
+y_data = [[0], [0], [0], [1], [1], [1]]
+x_train = torch.FloatTensor(x_data)
+y_train = torch.FloatTensor(y_data)
 
-plt.plot(x, y, 'g') #g는 색상
-plt.plot([0,0],[1.0,0.0], ':') # 가운데 점선 추가, [0,0],[1.0,0.0] = x축과 y축의 값을 지정 :은 점선 스타일
-plt.title('Sigmoid Function')
-plt.show()
+print(x_train.shape)
+print(y_train.shape)
