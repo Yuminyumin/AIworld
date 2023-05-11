c672f03b22c32c1c6d0c23483a31324dd28ea306
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-05-11 18:30:33 +0900

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -10,26 +10,28 @@ y_data = [[0], [0], [0], [1], [1], [1]]
 x_train = torch.FloatTensor(x_data)
 y_train = torch.FloatTensor(y_data)
 
-# 모델 초기화
-W = torch.zeros((2, 1), requires_grad=True)
-b = torch.zeros(1, requires_grad=True)
+model = nn.Sequential(
+   nn.Linear(2, 1), # input_dim = 2, output_dim = 1
+   nn.Sigmoid() # 출력은 시그모이드 함수를 거친다
+)
+
+
 # optimizer 설정
-optimizer = optim.SGD([W, b], lr=1)
+optimizer = optim.SGD(model.parameters(), lr=1)
 
 nb_epochs = 1000
 for epoch in range(nb_epochs + 1):
 
-    # Cost 계산
-    hypothesis = torch.sigmoid(x_train.matmul(W) + b)
-    cost = -(y_train * torch.log(hypothesis) + 
-             (1 - y_train) * torch.log(1 - hypothesis)).mean()
+    # H(x) 계산
+    hypothesis = model(x_train)
+
+    # cost 계산
+    cost = F.binary_cross_entropy(hypothesis, y_train)
 
     # cost로 H(x) 개선
     optimizer.zero_grad()
     cost.backward()
     optimizer.step()
 
-
-
-print(W)
-print(b)
+print(list(model.parameters()))
+        
