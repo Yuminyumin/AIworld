8577f3573aaaba305a2214ea2d48b14f753860fa
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-07-11 15:08:06 -0700

    Update import torch.py

--- import torch.py
+++ import torch.py
@@ -6,12 +6,13 @@ import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 from scipy.sparse import csr_matrix
 from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import SMOTE
+from collections import Counter
 
-# ---- Preprocessing
 # CSV 파일 로드
 data = pd.read_csv(r'c:\Users\신유민\Desktop\MBTI 500.csv', encoding='utf-8')
-s_data = data.sample(frac=1)  # 이건 샘플링하려고 shuffle
-s_data_s = s_data[1000:10000]
+s_data = data.sample(frac=1) #샘플링을 위한 shuffle
+s_data_s = s_data[1000:5000]
 
 # 텍스트 데이터와 레이블 분리
 X = s_data_s['posts']
@@ -21,23 +22,66 @@ y = s_data_s['type']
 label_encoder = LabelEncoder()
 label_y = label_encoder.fit_transform(y)
 
-# 학습 데이터와 테스트 데이터로 분할
-X_train, X_test, y_train, y_test = train_test_split(X, label_y, test_size=0.3, random_state=42)
+# 클래스별 샘플 개수 확인
+class_counts = Counter(label_y)
+print("Original class counts:", class_counts)
 
 # TF-IDF 벡터화 객체 생성
 tfidf_vectorizer = TfidfVectorizer()
-
-# 텍스트 데이터에 TF-IDF 적용
-train_tfidf_matrix = tfidf_vectorizer.fit_transform(X_train)
-test_tfidf_matrix = tfidf_vectorizer.transform(X_test)
+X_tfidf = tfidf_vectorizer.fit_transform(X)
 
 # 희소 행렬로 변환
-train_sparse_tfidf_matrix = csr_matrix(train_tfidf_matrix)
-test_sparse_tfidf_matrix = csr_matrix(test_tfidf_matrix)
+sparse_tfidf_matrix = csr_matrix(X_tfidf)
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+from scipy.sparse import csr_matrix
+from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+
+# CSV 파일 로드
+data = pd.read_csv(r'c:\Users\신유민\Desktop\MBTI 500.csv', encoding='utf-8')
+s_data = data.sample(frac=1) #샘플링을 위한 shuffle
+s_data_s = s_data[1000:4000]
+
+# 텍스트 데이터와 레이블 분리
+X = s_data_s['posts']
+y = s_data_s['type']
+
+# 레이블 인코딩
+label_encoder = LabelEncoder()
+label_y = label_encoder.fit_transform(y)
+
+# 클래스별 샘플 개수 확인
+class_counts = Counter(label_y)
+print("Original class counts:", class_counts)
+
+# TF-IDF 벡터화 객체 생성
+tfidf_vectorizer = TfidfVectorizer()
+X_tfidf = tfidf_vectorizer.fit_transform(X)
+
+# 희소 행렬로 변환
+sparse_tfidf_matrix = csr_matrix(X_tfidf)
+
+# SMOTE를 사용하여 클래스 불균형 보정
+smote = SMOTE(k_neighbors=5)
+X_resampled, label_y_resampled = smote.fit_resample(sparse_tfidf_matrix, label_y)
+
+# 조정된 클래스별 샘플 개수 확인
+resampled_class_counts = Counter(label_y_resampled)
+print("Resampled class counts:", resampled_class_counts)
+
+# 학습 데이터와 테스트 데이터로 분할
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, label_y_resampled, test_size=0.3, random_state=42)
 
 # XGBoost 학습용 데이터 생성
-dtrain = xgb.DMatrix(train_sparse_tfidf_matrix, label=y_train, enable_categorical=True)
-dtest = xgb.DMatrix(test_sparse_tfidf_matrix, label=y_test, enable_categorical=True)
+dtrain = xgb.DMatrix(X_train, label=y_train, enable_categorical=True)
+dtest = xgb.DMatrix(X_test, label=y_test, enable_categorical=True)
 
 # ----- Model
 num_class = len(label_encoder.classes_)
@@ -53,8 +97,8 @@ param_grid = {
 model = xgb.XGBClassifier(objective='multi:softmax', num_class=num_class, random_state=42)
 
 # GridSearchCV를 사용하여 하이퍼파라미터 튜닝
-grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=2, scoring='accuracy')
-grid_search.fit(train_sparse_tfidf_matrix, y_train)
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=4, scoring='accuracy')
+grid_search.fit(X_train, y_train)
 
 # 최적의 하이퍼파라미터 조합 출력
 print("Best Parameters:", grid_search.best_params_)
@@ -62,10 +106,10 @@ print("Best Score:", grid_search.best_score_)
 
 # 최적의 모델로 재학습
 best_model = grid_search.best_estimator_
-best_model.fit(train_sparse_tfidf_matrix, y_train)
+best_model.fit(X_train, y_train)
 
 # 예측
-preds = best_model.predict(test_sparse_tfidf_matrix)
+preds = best_model.predict(X_test)
 
 # 예측 결과 출력
 for idx, prediction in enumerate(preds):
@@ -75,3 +119,4 @@ for idx, prediction in enumerate(preds):
 # 정확도 계산
 accuracy = accuracy_score(y_test, preds)
 print(f'Accuracy: {accuracy}')
+
