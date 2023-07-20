d57f88c0b396bba82d68db3f3404c485d29f9f2c
신유민 <102581107+Yuminyumin@users.noreply.github.com>
2023-07-20 15:15:35 -0700

    ver3 제일 근사치

--- svc_ver3.py
+++ svc_ver3.py
@@ -1,27 +1,18 @@
 import pandas as pd
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.text import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import *
-from sklearn.linear_model import LogisticRegression
-from tqdm.notebook import tqdm
-import spacy
+from sklearn.metrics import accuracy_score
 from nltk.stem.snowball import SnowballStemmer
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from nltk.stem import SnowballStemmer
 from tqdm import tqdm
 
+# 데이터 불러오기
 train = pd.read_csv('c:\\Users\\신유민\\Desktop\\MBTI 500.csv', encoding='utf-8')
 test = train.drop(['type'], axis=1)
-test.head()
 
 # 설명변수
 X = train['posts']
@@ -29,43 +20,15 @@ X = train['posts']
 Y = train['type']
 
 print(f"{len(train['type'].unique())}개")
-train['type'].value_counts()
-train.isnull().sum()
-train['posts'].nunique() == len(train['posts'])
-print("train data : ", train.shape)
-print("test data : ", test.shape)
-
-# E, I 빈도수 확인
-first = []
-for i in range(len(train)):
-    first.append(train['type'][i][0])
-first = pd.DataFrame(first)
-first[0].value_counts()
-
-# N, S 빈도수 확인
-second = []
-for i in range(len(train)):
-    second.append(train['type'][i][1])
-second = pd.DataFrame(second)
-second[0].value_counts()
-
-# T, F 빈도수 확인
-third = []
-for i in range(len(train)):
-    third.append(train['type'][i][2])
-third = pd.DataFrame(third)
-third[0].value_counts()
-
-# P, J 빈도수 확인
-fourth = []
-for i in range(len(train)):
-    fourth.append(train['type'][i][3])
-fourth = pd.DataFrame(fourth)
-fourth[0].value_counts()
+print(train['type'].value_counts())
+print(train.isnull().sum())
+print(train['posts'].nunique() == len(train['posts']))
+print("train data:", train.shape)
+print("test data:", test.shape)
 
 # NLTK 불용어 처리를 위해 불용어 리스트 다운로드
-#nltk.download('stopwords')
-#nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('punkt')
 
 # NLTK에서 사용할 SnowballStemmer 로드
 s_stemmer = SnowballStemmer(language='english')
@@ -83,30 +46,43 @@ def replaceStemwords(s):
     return ' '.join(new_words)
 
 tqdm.pandas()
-train['posts'] = train['posts'].progress_apply(removeStopwords)
-train['posts'] = train['posts'].progress_apply(replaceStemwords)
-
-train.posts = train.posts.progress_apply(lambda x: removeStopwords(replaceStemwords(x)))
-
-# test data에도 적용
-test.posts = test.posts.progress_apply(lambda x: removeStopwords(replaceStemwords(x)))
+train['posts'] = train['posts'].progress_apply(removeStopwords)  # 불용어 처리 적용
+train['posts'] = train['posts'].progress_apply(replaceStemwords)  # 어간 추출 적용
 
 X = train.posts
-Y= train.type
+Y = train.type
 
 X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.2, random_state=1)
 
-text_clf = Pipeline([('tfidf',TfidfVectorizer()),('clf',LinearSVC(C=0.3))])
-text_clf.fit(X_train, Y_train)
+# TfidfVectorizer를 사용하여 텍스트 데이터를 벡터화
+vectorizer = TfidfVectorizer()
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_valid_tfidf = vectorizer.transform(X_valid)
+
+text_clf = Pipeline([('clf', LinearSVC(C=0.3))])
+text_clf.fit(X_train_tfidf, Y_train)
 
 # valid 데이터의 mbti 예측
-pred = text_clf.predict(X_valid)
-print("pred",pred)
+pred = text_clf.predict(X_valid_tfidf)
+print("pred", pred)
 
 # valid data에서의 정확도
 accuracy = accuracy_score(pred, Y_valid)
-print("accuracy",accuracy)
+print("accuracy", accuracy)
 
-# train 데이터프레임과 test 데이터프레임을 합친다고 가정
-data = pd.concat([train, test], axis=1)
-print(data.head())
+# 테스트 데이터에도 전처리 적용
+test['posts'] = test['posts'].progress_apply(removeStopwords) # 불용어 처리 적용
+test['posts'] = test['posts'].progress_apply(replaceStemwords)
+
+# 테스트 데이터를 벡터화
+X_test_tfidf = vectorizer.transform(test['posts'])
+
+# 테스트 데이터 예측
+test_pred = text_clf.predict(X_test_tfidf)
+
+# 예측 결과를 DataFrame으로 만듦
+predictions = pd.DataFrame({'type': test_pred})
+
+# 원래 테스트 데이터와 예측 결과를 합침
+result = pd.concat([test, predictions], axis=1)
+print(result.head())
