2cc69a13673f0856a21910d65307596c009a1f90
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2023-08-07 17:07:08 +0900

    feat: deepmodel

---
+++ deepLean.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# 데이터 불러오기
+train = pd.read_csv('c:\Users\user\Desktop\MBTI 500', encoding='utf-8')
+test = train.drop(['type'], axis=1)
+
+# 내용변수와 타입변수 설정
+X = train['posts']
+Y = train['type']
+
+# 훈련 데이터와 검증 데이터 분리
+X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+# 텍스트 전처리
+max_words = 5000  # 사용할 최대 단어 개수
+tokenizer = Tokenizer(num_words=max_words, oov_token="<OOV>")
+tokenizer.fit_on_texts(X_train)
+
+X_train_sequences = tokenizer.texts_to_sequences(X_train)
+X_valid_sequences = tokenizer.texts_to_sequences(X_valid)
+
+max_length = 200  # 시퀀스의 최대 길이
+X_train_padded = pad_sequences(X_train_sequences, maxlen=max_length, padding='post', truncating='post')
+X_valid_padded = pad_sequences(X_valid_sequences, maxlen=max_length, padding='post', truncating='post')
+
+# 딥러닝 모델 구축 (LSTM) with Hyperparameter Tuning
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(input_dim=max_words, output_dim=64, input_length=max_length),
+    tf.keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.2),  # 조정 가능한 하이퍼파라미터
+    tf.keras.layers.Dense(32, activation='relu'),  # 조정 가능한 하이퍼파라미터
+    tf.keras.layers.Dense(len(Y_train.unique()), activation='softmax')
+])
+#모델 컴파일
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 모델 훈련
+model.fit(X_train_padded, Y_train, epochs=15, batch_size=64, validation_data=(X_valid_padded, Y_valid))  # 조정 가능한 하이퍼파라미터
+
+# 검증 데이터에서의 예측 및 평가
+pred_probs = model.predict(X_valid_padded)
+pred_labels = np.argmax(pred_probs, axis=1)
+accuracy = accuracy_score(pred_labels, Y_valid)
+print("Accuracy:", accuracy)
