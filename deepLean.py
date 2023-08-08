fix : units
fix : units 16 씩

deepLean.py
@@ -10,6 +10,8 @@ from nltk.corpus import stopwords
 from tensorflow.keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+
 
 # 데이터 불러오기
 data1 = pd.read_csv('c:/Users/user/Desktop/mbtidata/mbti.csv', encoding='utf-8')  # mbti500
@@ -60,62 +62,54 @@ X_valid_padded = remove_stopwords(X_valid_padded)
 smote = SMOTE(random_state=0)
 X_train_resampled, Y_train_resampled = smote.fit_resample(X_train_padded, Y_train)
 
-# 병합한 데이터셋의 처음 5개 행 보기
-print("Head of the merged dataset:")
-print(all_data.head())
+#오버샘플링을 적용한 훈련 데이터 생성
+oversampler = RandomOverSampler(random_state=0)
+X_train_oversampled, Y_train_oversampled = oversampler.fit_resample(X_train_padded, Y_train)
 
-# 병합한 데이터셋의 마지막 5개 행 보기
-print("Tail of the merged dataset:")
-print(all_data.tail())
+# # 병합한 데이터셋의 처음 5개 행 보기
+# print("Head of the merged dataset:")
+# print(all_data.head())
 
-# 데이터프레임 생성 (예시)
-data = {'Column1': ['A', 'B', 'C'],
-        'Column2': [1, 2, 3]}
-df = pd.DataFrame(data)
+# # 병합한 데이터셋의 마지막 5개 행 보기
+# print("Tail of the merged dataset:")
+# print(all_data.tail())
 
-# 데이터프레임을 'utf-8' 인코딩으로 파일로 저장
-df.to_csv('output.csv', encoding='utf-8', index=False)
+# 딥러닝 모델 구축 (LSTM) with Hyperparameter Tuning
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(input_dim=max_words, output_dim=64, input_length=max_length),
+    tf.keras.layers.LSTM(units = 16, dropout=0.5, recurrent_dropout=0.2),                                                                       # 조정 가능한 하이퍼파라미터
+    tf.keras.layers.Dense(16, activation='relu'),                                                                                               # 조정 가능한 하이퍼파라미터
+    tf.keras.layers.Dense(len(label_encoder.classes_), activation='softmax') 
+])
 
-# # 딥러닝 모델 구축 (LSTM) with Hyperparameter Tuning
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Embedding(input_dim=max_words, output_dim=64, input_length=max_length),
-#     tf.keras.layers.LSTM(units = 32, dropout=0.5, recurrent_dropout=0.2),                                                                       # 조정 가능한 하이퍼파라미터
-#     tf.keras.layers.Dense(22, activation='relu'),                                                                                               # 조정 가능한 하이퍼파라미터
-#     tf.keras.layers.Dense(len(label_encoder.classes_), activation='softmax') 
-# ])
+# 모델 컴파일
+learning_rate = 0.01  # 수정 가능한 학습률 값
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# # 모델 컴파일
-# learning_rate = 0.01  # 수정 가능한 학습률 값
-# model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# 모델 훈련 전에 데이터 변환
+X_train_padded = np.array(X_train_padded)
+X_valid_padded = np.array(X_valid_padded)
+Y_train = np.array(Y_train)
 
-# # 모델 훈련 전에 데이터 변환
-# X_train_padded = np.array(X_train_padded)
-# X_valid_padded = np.array(X_valid_padded)
-# Y_train = np.array(Y_train)
+# Early Stopping 콜백 정의
+early_stopping = EarlyStopping(monitor='val_loss', patience=1, restore_best_weights=True)
 
-# # Early Stopping 콜백 정의
-# early_stopping = EarlyStopping(monitor='val_loss', patience=1, restore_best_weights=True)
+# 모델 훈련
+model.fit(X_train_oversampled, Y_train_oversampled, epochs=7, batch_size=32, validation_data=(X_valid_padded, Y_valid), callbacks=[early_stopping]) # 조정 가능한 하이퍼파라미터
 
-# #오버샘플링을 적용한 훈련 데이터 생성
-# oversampler = RandomOverSampler(random_state=0)
-# X_train_oversampled, Y_train_oversampled = oversampler.fit_resample(X_train_padded, Y_train)
+# 검증 데이터에서의 예측 및 평가
+pred_probs = model.predict(X_valid_padded)
+pred_labels = np.argmax(pred_probs, axis=1)
+accuracy = accuracy_score(pred_labels, Y_valid)
+print("Accuracy:", accuracy)
 
-# # 모델 훈련
-# model.fit(X_train_oversampled, Y_train_oversampled, epochs=7, batch_size=32, validation_data=(X_valid_padded, Y_valid), callbacks=[early_stopping]) # 조정 가능한 하이퍼파라미터
+# True Positive (TP)와 False Negative (FN) 계산
+TP = np.sum(np.logical_and(pred_labels == 1, Y_valid == 1))
+FN = np.sum(np.logical_and(pred_labels == 0, Y_valid == 1))
 
-# # 검증 데이터에서의 예측 및 평가
-# pred_probs = model.predict(X_valid_padded)
-# pred_labels = np.argmax(pred_probs, axis=1)
-# accuracy = accuracy_score(pred_labels, Y_valid)
-# print("Accuracy:", accuracy)
-
-# # True Positive (TP)와 False Negative (FN) 계산
-# TP = np.sum(np.logical_and(pred_labels == 1, Y_valid == 1))
-# FN = np.sum(np.logical_and(pred_labels == 0, Y_valid == 1))
-
-# # 재현율 계산
-# recall = TP / (TP + FN)
-# print("Recall:", recall)
+# 재현율 계산
+recall = TP / (TP + FN)
+print("Recall:", recall)
 
 # #클래스 개수 확인
 # class_counts = all_data['type'].value_counts()