fb042867c399a5b79eeebf83bb1830ebcbba933e
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2023-08-09 21:13:28 +0900

    fix : 결측치 제거
    fix : 결측치 제거 posts, type

--- svc_ver2.py
+++ svc_ver2.py
@@ -1,37 +1,49 @@
 import pandas as pd
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.text import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import *
-from sklearn.linear_model import LogisticRegression
-from tqdm.notebook import tqdm
-import spacy
 from nltk.stem.snowball import SnowballStemmer
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from nltk.stem import SnowballStemmer
 from tqdm import tqdm
+import pickle
 
-train = pd.read_csv('c:/Users/user/Desktop/mbtidata/mbti.csv', encoding='utf-8')
+# 데이터 불러오기
+train = pd.read_csv('c:/Users/user/Desktop/mbtidata/mbti_total.csv', encoding='latin1')
 test = train.drop(['type'], axis=1)
-test.head()
 
-# 설명변수
+# 설명변수(X)와 예측변수(Y) 설정
 X = train['posts']
-# 예측변수
 Y = train['type']
 
+# 유니크한 MBTI 유형 개수 확인
 print(f"{len(train['type'].unique())}개")
-train['type'].value_counts()
-train.isnull().sum()
+
+# 각 MBTI 유형별 데이터 개수 확인
+class_counts = train['type'].value_counts()
+print(class_counts)
+
+# 결측값을 확인합니다.
+print("결측값 수 (train.posts):", train.posts.isnull().sum())
+print("결측값 수 (train.type):", train.type.isnull().sum())
+
+# 결측치 제거
+train.dropna(subset=['type'], inplace=True)
+train.dropna(subset=['posts'], inplace=True)
+
+# 결측값을 재확인합니다.
+print("결측값 수 (train.posts):", train.posts.isnull().sum())
+print("결측값 수 (train.type):", train.type.isnull().sum())
+
+# 중복되지 않은 게시글 개수와 전체 게시글 수 비교
 train['posts'].nunique() == len(train['posts'])
+
+# 훈련 데이터와 테스트 데이터 형태 확인
 print("train data : ", train.shape)
 print("test data : ", test.shape)
 
@@ -42,44 +54,71 @@ print("test data : ", test.shape)
 # NLTK에서 사용할 SnowballStemmer 로드
 s_stemmer = SnowballStemmer(language='english')
 
+# 불용어 제거 함수
 def removeStopwords(s):
-    stop_words = set(stopwords.words('english'))
-    words = word_tokenize(s)
-    new_words = [word for word in words if word.lower() not in stop_words]
-    return ' '.join(new_words)
+    if isinstance(s, str):  # 문자열인 경우에만 처리
+        stop_words = set(stopwords.words('english'))
+        words = word_tokenize(s)
+        new_words = [word for word in words if word.lower() not in stop_words]
+        return ' '.join(new_words)
+    else:
+        return s  # NaN 또는 비문자열 값인 경우 그대로 반환
 
 # 어간 추출 함수
 def replaceStemwords(s):
-    words = word_tokenize(s)
-    new_words = [s_stemmer.stem(word) for word in words]
-    return ' '.join(new_words)
+    if isinstance(s, str):  # 문자열인 경우에만 처리
+        words = word_tokenize(s)
+        new_words = [s_stemmer.stem(word) for word in words]
+        return ' '.join(new_words)
+    else:
+        return s  # NaN 또는 비문자열 값인 경우 그대로 반환
 
+# 진행 상황 표시를 위한 tqdm 설정
 tqdm.pandas()
+
+# 불용어 제거 및 어간 추출 적용
 train['posts'] = train['posts'].progress_apply(removeStopwords)
 train['posts'] = train['posts'].progress_apply(replaceStemwords)
-
 train.posts = train.posts.progress_apply(lambda x: removeStopwords(replaceStemwords(x)))
 
-# test data에도 적용
+# 테스트 데이터에도 불용어 제거 및 어간 추출 적용
 test.posts = test.posts.progress_apply(lambda x: removeStopwords(replaceStemwords(x)))
 
-X = train.posts
-Y= train.type
+# 훈련 데이터의 설명변수(X)와 예측변수(Y) 설정
+X_processed = train.posts.fillna('', inplace=False).replace({r'\s+$': '', r'^\s+': ''}, regex=True)  # 빈 문자열로 NaN 값을 대체하고 불필요한 공백 제거
+Y = train.type
 
-X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.2, random_state=1)
+# 훈련 데이터와 검증 데이터 분리
+X_train, X_valid, Y_train, Y_valid = train_test_split(X_processed, Y, test_size=0.2, random_state=1)
 
-#모델 훈련
+# 선형 SVM 모델을 사용한 텍스트 분류를 위한 Pipeline 생성 및 모델 훈련
 text_clf = Pipeline([('tfidf',TfidfVectorizer()),('clf',LinearSVC(C=0.3))])
 text_clf.fit(X_train, Y_train)
 
+# 학습된 모델을 pickle 파일로 저장
+save_path = 'c:/Users/user/Desktop/mbtidata/MBTIgram.pkl'
+with open('MBTIgram.pkl', 'wb') as model_file:
+    pickle.dump(text_clf, model_file)
+
+# 저장된 모델을 다시 로드하여 예측 수행
+with open('MBTIgram.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
+
 # valid 데이터의 mbti 예측
-pred = text_clf.predict(X_valid)
+pred = loaded_model.predict(X_valid)
 print("pred",pred)
 
 # valid data에서의 정확도
 accuracy = accuracy_score(pred, Y_valid)
 print("accuracy",accuracy)
 
-# # train 데이터프레임과 test 데이터프레임을 합친다고 가정
-# data = pd.concat([train, test], axis=1)
-# print(data.head())
+#클래스 개수 확인
+class_counts = train['type'].value_counts()
+print(class_counts)
+
+#클래스 불균형 시각화
+class_counts.plot(kind='bar')
+plt.xlabel('Class')
+plt.ylabel('Count')
+plt.title('Class Distribution')
+plt.show()
